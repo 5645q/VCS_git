e2ca979356ec7a393f4046f19b83218750aa4ac8
1282079624 <1282079624@qq.com>
2020-06-01 10:32:02 +0800

    implement:功能实现

--- app/src/main/java/com/mamba/vcs_git/MainActivity.java
+++ app/src/main/java/com/mamba/vcs_git/MainActivity.java
@@ -3,12 +3,21 @@ package com.mamba.vcs_git;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button1,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.i("TAG",1+"");
+            }
+        });
     }
 }
